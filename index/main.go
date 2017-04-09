e89b289fd201c1dab1ae2266cc6bae9c5a75cdd1
stgleb <glebstepanov1992@gmail.com>
2017-04-09 23:09:14 +0300

    Add methods for index

--- index/main.go
+++ index/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"flag"
+)
 
+func ReadIndex(fileName string) (map[string][]string) {
+	return nil
+}
+
+func UpdateIndex(fileName string, index map[string][]string) map[string][]string {
+	return nil
+}
+
+func WriteIndex(indexFile string, index map[string][]string) {
+
+}
+
+func main() {
+	flag.Parse()
+	fileName := *flag.String("file", "file.txt", "File name")
+	indexFile := *flag.String("index", "index.dat", "Index file name")
+	index := ReadIndex(indexFile)
+	index = UpdateIndex(fileName, index)
+	WriteIndex(indexFile, index)
 }
